43365e849ec421c39b0c85060c3bb25546c7a9ed
TatianaYakushonok <yakush2@yandex.ru>
2023-04-17 12:59:46 +0700

    feat: add task stack (Mon, 17 Apr 2023 13:04:08 +0700)

--- src/stack.js
+++ src/stack.js
@@ -13,21 +13,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(...arr) {
+    this.arr = new Array();
+
+    if (this.arr.length > 0) {
+      arr.forEach(element => this.arr.push(element));
+    }
+  }
+
+  push(...arr) {
+    arr.forEach(element => this.arr.push(element) );
+    return this.arr;
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr[this.arr.length-1];
   }
 }
+const stack = new Stack();
+stack.push(1);
+stack.peek();
+stack.pop();
+stack.pop();
+//stack.pop();
+console.log(stack);
 
 module.exports = {
   Stack
